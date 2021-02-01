02ce78a009c4bbfe5d8707840affd97c2dbfc1af
gk-svg <gk32239@gmail.com>
2021-02-01 16:18:13 +0530

    initializing STL array

---
+++ STL_Containers/STL_array.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+#include<array>
+using namespace std;
+
+int main(){
+    std::array<int ,3> intarray {1,2,3};
+    std::array<int ,5> intarray2={1,2,3};
+    std::array<int ,4> intarray3;
+    intarray3={1,2,3};
+    std::array<char ,3> chararray {'a','b','c'};
+    return 0;
+}
